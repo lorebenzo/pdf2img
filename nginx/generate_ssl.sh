376c54406a2f9c61725f784918b5fc1ae3cc794c
lorenzobenzoni <lorenzo1.benzoni@mail.polimi.it>
2025-02-15 12:28:59 +0100

    refactored nginx docker

--- nginx/generate_ssl.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
-    -keyout /etc/nginx/ssl/selfsigned.key \
-    -out /etc/nginx/ssl/selfsigned.crt \
-    -subj "/C=US/ST=State/L=City/O=Company/CN=localhost"
